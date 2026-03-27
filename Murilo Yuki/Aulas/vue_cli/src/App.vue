868f90b5bf767f7032ae471112ba4bb15969ee8a
Murilo Yuki Sukiyama Pinto <muriloyukisukiyama21@dinfor.uniceub>
2026-03-26 21:09:48 -0300

    Vue Cli novas adições

--- Murilo Yuki/Aulas/vue_cli/src/App.vue
+++ Murilo Yuki/Aulas/vue_cli/src/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Meu App Vue</h1>
+    <p class="subtitulo">Meu subtitulo</p>
     <br />
     <home-component />
     <hr />
+    <br />
+    <usuario-component />
+    <hr />
+    <br />
+    <estilo-component/>
   </div>
 </template>
 
 <script>
 import HomeComponent from './components/HomeComponent.vue';
+import UsuarioComponent from './components/UsuarioComponent.vue';
+import EstiloComponent from './components/EstiloComponent.vue';
+
 
 
 export default {
   name: 'App',
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+created(){
+  console.log("created")
+  },
+mounted(){
+  console.log("mouted")
+  },
+updated(){
+  console.log("updated")
+  },
+errorCaptured(){
+  console.log("erro")
   },
 };
+
+
 </script>
 
 <style>
@@ -27,5 +53,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: darkblue;
+  text-align: center;
+}
+.subtitulo{
+  color: chocolate;
+  font-family: "Gill Sans";
+  text-align: center;
 }
 </style>
